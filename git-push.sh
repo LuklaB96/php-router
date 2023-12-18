9edd65d596c90dcc9ff10c8c2a642938756b0a5a
Lukasz <lukasz.bulicz96@gmail.com>
2023-12-18 09:56:14 +0100

    remove script

--- git-push.sh
+++ git-push.sh
@@ -2,7 +2,6 @@ echo "Enter your message"
 read message
 git add .
 git commit -m"${message}"
- git status
- echo "Pushing data to remote server!!!"
- git push -u origin master
-fi
+git status
+echo "Pushing data to remote server"
+git push -u origin master
